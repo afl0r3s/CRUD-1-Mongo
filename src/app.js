390550e141a068a58dca20e4e828c10fcf70801a
Alejandro Flores <sjafs.83@gmail.com>
2021-08-29 22:19:17 -0300

    02 another files created

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
+const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 //import routes
+require('./routes/index')
 
 //settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
 
 //midleware
+app.use(morgan('dev'));
+
+//routes
 
 //starting server
-app.listen(app.get('port'), ()=> {
-    console.log('Server on port 3000')
-})
+app.listen(app.get('port'), () => {
+	console.log(`Server on port: ${app.get('port')}`);
+});
